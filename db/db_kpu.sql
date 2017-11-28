1409df517f36b5fce60f0f7509cb29a554cf864d
aanggoro <1511501288@student.budiluhur.ac.id>
2017-11-28 13:36:59 +0700

    ganti db

--- db/db_kpu.sql
+++ db/db_kpu.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 18, 2017 at 08:25 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.16
+-- Generation Time: 28 Nov 2017 pada 07.35
+-- Versi Server: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -25,17 +25,16 @@ USE `db_kpu`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struktur dari tabel `admin`
 --
 
 CREATE TABLE IF NOT EXISTS `admin` (
   `username` varchar(10) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  PRIMARY KEY (`username`)
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `admin`
+-- Dumping data untuk tabel `admin`
 --
 
 INSERT INTO `admin` (`username`, `password`) VALUES
@@ -44,25 +43,75 @@ INSERT INTO `admin` (`username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pemilih`
+-- Struktur dari tabel `partai`
+--
+
+CREATE TABLE IF NOT EXISTS `partai` (
+`id_partai` tinyint(2) NOT NULL,
+  `nama_partai` varchar(50) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data untuk tabel `partai`
+--
+
+INSERT INTO `partai` (`id_partai`, `nama_partai`) VALUES
+(1, 'partai segitiga'),
+(2, 'partai lingkaran'),
+(3, 'partai kotak');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemilih`
 --
 
 CREATE TABLE IF NOT EXISTS `pemilih` (
   `nim` int(10) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `fakultas` varchar(50) NOT NULL,
-  `partai` varchar(50) NOT NULL,
-  PRIMARY KEY (`nim`)
+  `partai` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `pemilih`
+-- Dumping data untuk tabel `pemilih`
 --
 
 INSERT INTO `pemilih` (`nim`, `nama`, `fakultas`, `partai`) VALUES
-(1411503293, 'Arief Setiadi', 'Fakultas Teknologi Informasi', 'Partai Apel'),
-(1411503533, 'Freddy Saleh', 'Fakultas Teknologi Informasi', 'Partai Bawang');
+(1511501015, 'Soleh Hudin', 'Fakultas Teknologi Informasi', '1'),
+(1511501288, 'achmad anhar anggoro', 'Fakultas Teknologi Informasi', '1');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+ ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `partai`
+--
+ALTER TABLE `partai`
+ ADD PRIMARY KEY (`id_partai`);
+
+--
+-- Indexes for table `pemilih`
+--
+ALTER TABLE `pemilih`
+ ADD PRIMARY KEY (`nim`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `partai`
+--
+ALTER TABLE `partai`
+MODIFY `id_partai` tinyint(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
